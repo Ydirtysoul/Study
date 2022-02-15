cc3760255d618573b289d46cf014f5165cdb2f68
Ydirtysoul <kkauderer@meta-devs.io>
2022-02-15 21:32:39 +0300

    Lesson 1 done

---
+++ src/Lesson1/Lesson1.java
@@ -0,0 +1,19 @@
+package Lesson1;
+
+import java.util.Scanner;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Ну, здравствуй World!\n");
+        int value1 = 0;
+        int value2 = 5;
+        int sum = value1 + value2;
+        System.out.println("Сумма " + sum);
+        int multiply = value1 * value2;
+        System.out.println("Произведение " + multiply);
+        int difference = value1 - value2;
+        System.out.println("Разность " + difference);
+        String scanner = (new Scanner(System.in)).next();
+        System.out.println(scanner);
+    }
+}
